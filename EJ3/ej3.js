633d12973596f76f5d9849b1f8d9f4de41530c49
Daniel <daniel.soto.ortiz@gmail.com>
2024-10-18 20:19:01 +0200

    second

---
+++ EJ3/ej3.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+
+
+    function ordenarFecha(a, b) {
+
+        const fechaA = new Date(a.fechaConsecucion.split('/').reverse().join('-'));
+        const fechaB = new Date(b.fechaConsecucion.split('/').reverse().join('-'));
+
+        
+        return fechaA - fechaB;
+
+
+    }
+
+    //Creamos un nuevo array ordenado por fecha de Consecución;
+    let reconocimientosOrdenados = alumnosReconocidos.toSorted(ordenarFecha);
+
+   
+    //Array que imprimiremos
+    let final=[];
+
+
+    reconocimientosOrdenados.forEach(element => {
+        final.push({nombre: element.nombre, total: element.alumnos.length})
+        //El método push añade elementos al array
+        
+    }
+    );
+
+    console.table(final)
+
+
+
+}
